1fba22a493980b52250e555a8f7035b93b4d255f
Robert Ackermann <robert_ackermann@icloud.com>
2023-06-24 14:10:01 +0200

    implemented basic 404 error page

--- Program.cs
+++ Program.cs
@@ -67,6 +67,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404)
+    {
+        context.Request.Path = "/404";
+        await next();
+    }
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
